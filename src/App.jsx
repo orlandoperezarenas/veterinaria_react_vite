42dc0397fcc464f486fa47fceeab5b90173b3565
Orlando <sorlando1989@gmail.com>
2023-07-10 23:38:20 -0300

    Fixing missing issue

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,31 @@ function App() {
 const [pacientes, setPacientes] = useState([]) // for add part
 const [paciente, setPaciente] = useState([]) //for edition part
 
+useEffect(()=>{
+   console.log('First effects') 
+   const obtenerLocalStorage=()=>{
+    const pacientesLocalStorage= JSON.parse(localStorage.getItem('pacientes'))??[]
+    setPacientes(pacientesLocalStorage)
+
+    console.log(pacientesLocalStorage, 'pacientesLocalStorage')
+   }
+   obtenerLocalStorage()
+},[])
+
+useEffect(()=>{
+  localStorage.setItem('pacientes', JSON.stringify(pacientes))
+    console.log('cambio pacientes')
+},[pacientes])
+
+const eliminarPaciente =(id)=>{
+  const pacientesAct = pacientes.filter((paciente)=>{
+    return paciente.id != id
+  })
+
+  setPacientes(pacientesAct)
+  // console.log(pacientesAct, 'pacientesAct')
+}
+
   return (
     <div className="container mx-auto mt-20">
       <Header
@@ -22,6 +47,7 @@ const [paciente, setPaciente] = useState([]) //for edition part
         <ListadoPacientes
           pacientes={pacientes}
           setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
         />
       </div>
       
