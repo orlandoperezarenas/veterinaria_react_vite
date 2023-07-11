2839061586f30abd06347f2061f5b8aec456b410
Orlando <sorlando1989@gmail.com>
2023-07-10 23:46:22 -0300

    Fixing issue2?

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import Formulario from './components/Formulario'
 import ListadoPacientes from './components/ListadoPacientes'
 
